e10951a51907fed1a1b593ba01e4f9046392fcd9
MINHEEKANG <queen.minhee@gmail.com>
2020-02-11 22:04:57 +0900

    [ADD] Brute Force

---
+++ BOJ/BRUTE FORCE/Main2231_강민희.java
@@ -0,0 +1,34 @@
+package Day03_BruteForce;
+
+import java.util.Scanner;
+
+public class Test2231 {
+	//Åë°ú ¾ÈµÊ... Ã£¾ÆºÁ¾ß°Ú´Ù..
+	public static void main(String[] args) {
+		/*
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		int m[] = new int [1000000];
+		
+		for(int i = 0; i<10; i++) {
+			for(int j =0 ; j<10; j++) {
+				for(int k =0 ; k<10; k++) {
+					 if(n == (100*i + 10*j + k) + i + j + k) {
+						 m[i] = 100*i + 10*j + k;
+						 int min = m[1];
+						 for(int r=1;r<i; r++) {
+							 if(m[r]<=min) {
+								 min = m[r];
+								 System.out.println(min);
+								 break;
+							 }
+						 }
+					 }
+				}
+			}
+		}
+		*/
+		
+	}
+}
