69874588d15500a902b8e600a3c4073a774c1d9d
daniel0458 <daniel0458@hanmail.net>
2020-02-23 17:23:49 +0900

    성현 다시 올림

--- BOJ/STACK/Main4949_seonghyeon.java
+++ BOJ/STACK/Main4949_seonghyeon.java
@@ -5,72 +5,39 @@ import java.util.Stack;
 
 public class Main4949_seonghyeon {
 	public static void main(String[] args) {
-		int t = 0;
 		Scanner sc = new Scanner(System.in);
-		while (t == 0) {
-			boolean tf= true;
+		while (true) {
 			String str = sc.nextLine();
 			if (str.equals(".")) {
+				sc.close();
 				return;
 			}
-			Stack<Character> s = new Stack<Character>();
-			for (int i = 0; i < str.length(); i++) {
-				char index = str.charAt(i);
-				if (index == '(' || index == '[') {
-					s.push(index);
-				} else if (index == ')') {
-					if (s.isEmpty() || s.pop() != '(') {
-//						return "NO";
-						tf=false;
-						break;
-					}
-				} else if (index == ']') {
-					if (s.isEmpty() || s.pop() != '[') {
-//						return "NO";
-						tf=false;
-						break;
-					}
-				}
-			}
-			if (!s.isEmpty()) {
-//				return "NO";
-				tf=false;
-			}
-			
-//			return "YES";
-			if(tf == false) {
-				System.out.println("NO");
-			}
-			else { 
-				System.out.println("YES");
-			}
-//			System.out.println(isBalacne(str));
+			System.out.println(isBalacne(str));
 		}
-		sc.close();
 	}
 
-//	private static String isBalacne(String str) {
-//		Stack<Character> s = new Stack<Character>();
-//		for (int i = 0; i < str.length(); i++) {
-//			char index = str.charAt(i);
-//			if (index == '(' || index == '[') {
-//				s.push(index);
-//			} else if (index == ')') {
-//				if (s.isEmpty() || s.pop() != '(') {
-//					return "NO";
-//				}
-//			} else if (index == ']') {
-//				if (s.isEmpty() || s.pop() != '[') {
-//					return "NO";
-//				}
-//			}
-//		}
-//		
-//		if (!s.isEmpty()) {
-//			return "NO";
-//		}
-//		
-//		return "YES";
-//	}
+	private static String isBalacne(String str) {
+		Stack<Character> s = new Stack<Character>();
+		for (int i = 0; i < str.length(); i++) {
+			char index = str.charAt(i);
+			if (index == '(' || index == '[') {
+				s.push(index);
+			} else if (index == ')') {
+				if (s.isEmpty() || s.pop() != '(') {
+					return "NO";
+				}
+			} else if (index == ']') {
+				if (s.isEmpty() || s.pop() != '[') {
+					return "NO";
+				}
+			}
+		}
+
+		if (!s.isEmpty()) {
+			return "NO";
+		}
+
+		return "YES";
+	}
 
 }
