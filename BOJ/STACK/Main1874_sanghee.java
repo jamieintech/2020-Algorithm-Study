e51546953eaf4eb57824a07fa43463fe66203355
ksang95 <ksanghe2@naver.com>
2020-02-23 14:43:44 +0900

    [update] stack(불필요한 import제거) -상희

--- BOJ/STACK/Main1874_sanghee.java
+++ BOJ/STACK/Main1874_sanghee.java
@@ -1,10 +1,8 @@
 package stack;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main1874_sanghee {
