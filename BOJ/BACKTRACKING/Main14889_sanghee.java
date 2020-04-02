27541550a43f945569e5e9f962f3c3b49636f5b4
ksang95 <ksanghe2@naver.com>
2020-04-02 19:43:59 +0900

    [UPDATE] backtracking 상희

--- BOJ/BACKTRACKING/Main14889_sanghee.java
+++ BOJ/BACKTRACKING/Main14889_sanghee.java
@@ -1,41 +1,66 @@
 package backtracking;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main14889_sanghee {
 
-	public static int findMinDiff(int arr[][], boolean selected[], int index, int start, int link) {
-		if (index == arr.length)
+	public static int findMinDiff(int arr[][], boolean selected[], int index, int count) {
+		if (count == arr.length / 2 || index - count == arr.length / 2) {
+			boolean temp = true;
+			if (count == arr.length / 2) {
+				temp = false;
+			}
+			for (int i = index; i < arr.length; i++) {
+				selected[i] = temp;
+			}
+
+			int start = 0;
+			int link = 0;
+			for (int i = 0; i < arr.length - 1; i++) {
+				for (int j = i + 1; j < arr.length; j++) {
+					if (selected[i] && selected[j])
+						start += arr[i][j] + arr[j][i];
+					else if (!selected[i] && !selected[j])
+						link += arr[i][j] + arr[j][i];
+				}
+			}
 			return start > link ? start - link : link - start;
-			
-		int temp1 = start;
-		int temp2 = link;
-		for (int j = 0; j < index; j++) {
-			if (selected[j])
-				temp1 += arr[index][j] + arr[j][index];
-			else
-				temp2 += arr[index][j] + arr[j][index];
 		}
+
 		selected[index] = true;
-		int diff1 = findMinDiff(arr, selected, index + 1, temp1, link);
+		int diff1 = findMinDiff(arr, selected, index + 1, count + 1);
 		selected[index] = false;
-		int diff2 = findMinDiff(arr, selected, index + 1, start, temp2);
-		return diff1<diff2?diff1:diff2;
+		int diff2 = findMinDiff(arr, selected, index + 1, count);
+		return diff1 < diff2 ? diff1 : diff2;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int arr[][] = new int[n][n];
-		boolean selected[] = new boolean[n];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				arr[i][j] = s.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n;
+		try {
+			n = Integer.parseInt(br.readLine());
+			int arr[][] = new int[n][n];
+			boolean selected[] = new boolean[n];
+			for (int i = 0; i < n; i++) {
+				String line=br.readLine();
+				StringTokenizer st=new StringTokenizer(line," ");
+				for (int j = 0; j < n; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
 			}
+			
+			System.out.println(findMinDiff(arr, selected, 0, 0));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		System.out.println(findMinDiff(arr, selected, 0, 0, 0));
 
 	}
 
--- BOJ/BACKTRACKING/Main14889_sanghee2.java
+++ BOJ/BACKTRACKING/Main14889_sanghee2.java
@@ -1,43 +1,65 @@
 package backtracking;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main14889_sanghee2 {
 
-	public static int findMinDiff(int arr[][], boolean selected[], int index, int count) {
-		if (count == arr.length / 2 || index - count == arr.length / 2) {
-			int start = 0;
-			int link = 0;
-			for (int i = 0; i < arr.length-1; i++) {
-				for (int j = i+1; j < arr.length; j++) {
-					if (selected[i] && selected[j])
-						start += arr[i][j] + arr[j][i];
-					else if (!selected[i] && !selected[j])
-						link += arr[i][j] + arr[j][i];
-				}
-			}
-			return start > link ? start - link : link - start;
+	static int min = 10000;
+
+	public static void findMinDiff(int arr[][], boolean selected[], int index, int count, int start, int link) {
+		if (index == arr.length) {
+			int diff = start > link ? start - link : link - start;
+			if (diff < min)
+				min = diff;
+			return;
 		}
-		selected[index] = true;
-		int diff1 = findMinDiff(arr, selected, index + 1, count + 1);
-		selected[index] = false;
-		int diff2 = findMinDiff(arr, selected, index + 1, count);
-		return diff1 < diff2 ? diff1 : diff2;
+
+		int temp1 = 0;
+		int temp2 = 0;
+		for (int i = 0; i < index; i++) {
+			if (selected[i])
+				temp1 += arr[i][index] + arr[index][i];
+			else
+				temp2 += arr[i][index] + arr[index][i];
+		}
+		if (count < arr.length / 2) {
+			selected[index] = true;
+			findMinDiff(arr, selected, index + 1, count + 1, start + temp1, link);
+		}
+		if (index - count < arr.length / 2) {
+			selected[index] = false;
+			findMinDiff(arr, selected, index + 1, count, start, link + temp2);
+		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int arr[][] = new int[n][n];
-		boolean selected[] = new boolean[n];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				arr[i][j] = s.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n;
+		try {
+			n = Integer.parseInt(br.readLine());
+			int arr[][] = new int[n][n];
+			boolean selected[] = new boolean[n];
+			for (int i = 0; i < n; i++) {
+				String line = br.readLine();
+				StringTokenizer st = new StringTokenizer(line, " ");
+				for (int j = 0; j < n; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
 			}
-		}
 
-		System.out.println(findMinDiff(arr, selected, 0, 0));
+			findMinDiff(arr, selected, 0, 0, 0, 0);
+			System.out.println(min);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
